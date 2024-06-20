3e9828fef7fee9ee188c25d31d32980b94d7dc22
Steve <34465153+xxl4@users.noreply.github.com>
2024-06-20 10:36:11 +0800

    add request id

---
+++ src/Http/Middleware/AssignRequestId.php
@@ -0,0 +1,31 @@
+<?php
+ namespace NexaMerchant\Apis\Http\Middleware;
+ 
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
+use Symfony\Component\HttpFoundation\Response;
+ 
+class AssignRequestId
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $requestId = (string) Str::uuid();
+ 
+        Log::withContext([
+            'request-id' => $requestId
+        ]);
+ 
+        $response = $next($request);
+ 
+        $response->headers->set('Request-Id', $requestId);
+ 
+        return $response;
+    }
+}
