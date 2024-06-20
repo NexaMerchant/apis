de29deecccf5ea202a5c372098b8d6fc6e125f21
Steve <34465153+xxl4@users.noreply.github.com>
2024-06-20 13:33:43 +0800

    add src config

--- src/Config/l5-swagger.php
+++ src/Config/l5-swagger.php
@@ -41,7 +41,7 @@ return [
                  * Absolute paths to directory containing the swagger annotations are stored.
                 */
                 'annotations' => [
-                    base_path('vendor/nexa-merchant/api/src/Docs/Shop'),
+                    base_path('vendor/nexa-merchant/apis/src/Docs/Shop'),
                 ],
 
             ],
@@ -84,7 +84,7 @@ return [
                  * Absolute paths to directory containing the swagger annotations are stored.
                 */
                 'annotations' => [
-                    base_path('vendor/nexa-merchant/api/src/Docs/Admin'),
+                    base_path('vendor/nexa-merchant/apis/src/Docs/Admin'),
                 ],
 
             ],
