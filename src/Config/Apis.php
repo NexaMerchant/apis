2af943b4c1e3e1edfc1b8b56bbb4b106414b99ed
Steve <34465153+xxl4@users.noreply.github.com>
2024-07-18 10:01:48 +0800

    #4 add desc for config key

--- src/Config/Apis.php
+++ src/Config/Apis.php
@@ -1,8 +1,30 @@
 <?php
 return [
+    /**
+     * 
+     * The name of the package
+     */
     'name' => 'Apis',
+    /**
+     * 
+     * The version of the package
+     */
     'version' => '1.0.7',
+    /**
+     * 
+     * The version number of the package
+     */
     'versionNum' => '107',
+    /**
+     * 
+     * enable input log for all requests default is false
+     * 
+     */
     'enable_input_log' => env("APIS_ENABLE_INPUT_LOG", false), // Enable input log for all requests
+    /**
+     * 
+     * enable output log for all requests default is false
+     * 
+     */
     'enable_output_log' => env("APIS_ENABLE_OUTPUT_LOG", false), // Enable output log for all requests
 ];
