4836afa084991f7f09d15749d4d8a40eac4988fa
Steve <34465153+xxl4@users.noreply.github.com>
2024-08-13 18:12:49 +0800

    fix apis version

--- src/Docs/V1/Admin/Controllers/Controller.php
+++ src/Docs/V1/Admin/Controllers/Controller.php
@@ -4,7 +4,7 @@ namespace NexaMerchant\Apis\Docs\V1\Admin\Controllers;
 
 /**
  * @OA\Info(
- *      version="1.1.0",
+ *      version="1.1.1",
  *      title="NexaMerchant Admin Rest API Documentation",
  *      description="NexaMerchant Admin Rest API Documentation",
  *
