8a938b7699c62ba16dc92a45351ffcfdaae24aa2
Steve <34465153+xxl4@users.noreply.github.com>
2024-09-06 10:11:49 +0800

    add v2 router

---
+++ src/Routes/V2/api.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Shop routes.
+ */
+require 'Shop/shop.php';
+
+/**
+ * Admin routes.
+ */
+require 'Admin/admin.php';
+
+/**
+ * APP routes.
+ */
+require 'App/app.php';
--- src/Routes/api.php
+++ src/Routes/api.php
@@ -4,3 +4,8 @@
  * APIs for version 1.
  */
 require 'V1/api.php';
+
+/**
+ * APIs for version 2.
+ */
+require 'V2/api.php';
