95f5e811de3ae2646324710627e5dc7768d12e66
Steve <34465153+xxl4@users.noreply.github.com>
2024-11-21 16:38:05 +0800

    add system upload docs

---
+++ src/Docs/V1/Admin/Controllers/System/TinyMCEController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace NexaMerchant\Apis\Docs\V1\Admin\Controllers\System;
+
+class TinyMCEController
+{
+    /**
+     * @OA\Post(
+     *      path="/api/v1/admin/system/tinymce/upload",
+     *      operationId="uploadTinyMCE",
+     *      tags={"TinyMCE"},
+     *      summary="Upload file from TinyMCE",
+     *      description="Upload file from TinyMCE",
+     *      security={ {"sanctum_admin": {} }},
+     *
+     *      @OA\RequestBody(
+     *          required=true,
+     *          description="Upload file from TinyMCE",
+     *          @OA\MediaType(
+     *              mediaType="multipart/form-data",
+     *              @OA\Schema(
+     *                  @OA\Property(
+     *                      property="file",
+     *                      description="File to upload",
+     *                      type="file",
+     *                  ),
+     *              )
+     *          )
+     *      ),
+     *
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\JsonContent(
+     *              @OA\Property(
+     *                  property="location",
+     *                  type="string",
+     *                  description="File location",
+     *              ),
+     *          ),
+     *      ),
+     * )
+     */
+    public function upload()
+    {
+    }
+}
