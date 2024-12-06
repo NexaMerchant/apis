18f2c49fe0b622a2bb8cb0a33808ad39aec3fe00
Steve <34465153+xxl4@users.noreply.github.com>
2024-12-06 19:28:24 +0800

    add api return code for project

---
+++ src/Enum/ApiCode.php
@@ -0,0 +1,12 @@
+<?php
+namespace NexaMerchant\Apis\Enum;
+
+use BenSampo\Enum\Enum;
+
+class ApiCode extends Enum
+{
+    const ORDER = 1000;
+    const PRODUCT = 2000;
+    const USER = 3000;
+    const SYSTEM = 4000;
+}
